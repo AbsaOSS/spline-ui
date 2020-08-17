5e7ac4483e16ceed2bb0abc36e5de585fbfebfc1
Oleksandr Korchak <clparenb@gmail.com>
2020-08-17 13:43:36 +0200

    [Deployment] :: max packages size.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,22 +21,12 @@ import { MatTooltipModule } from '@angular/material/tooltip'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { environment } from '@env/environment'
-import { EffectsModule } from '@ngrx/effects'
-import { StoreRouterConnectingModule } from '@ngrx/router-store'
-import { MetaReducer, StoreModule } from '@ngrx/store'
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'
-import { storeFreeze } from 'ngrx-store-freeze'
 import { SplineLayoutModule, SplineSearchBoxModule, SplineTranslateCoreModule } from 'spline-common'
 import { SplineAttributeSearchModule, SplineConfigModule, SplineConfigSettings, SPLINE_CONFIG_SETTINGS } from 'spline-shared'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 
-
-export const metaReducers: MetaReducer<any>[] =
-    !environment.production
-        ? [storeFreeze]
-        : []
 
 @NgModule({
     declarations: [
@@ -47,15 +37,6 @@ export const metaReducers: MetaReducer<any>[] =
         BrowserAnimationsModule,
         SplineTranslateCoreModule,
         AppRoutingModule,
-        // NGRX store
-        StoreModule.forRoot(
-            {}, { metaReducers },
-        ),
-        !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 100 }) : [],
-        EffectsModule.forRoot([]),
-        StoreRouterConnectingModule.forRoot({
-            stateKey: 'router',
-        }),
         SplineLayoutModule,
         SplineSearchBoxModule,
         SplineConfigModule,
