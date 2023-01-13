8cbaad36566f723767465e0638f0ef36208fa4e0
Oleksandra Kalinina <946369+korel-san@users.noreply.github.com>
2023-01-13 20:16:04 +0200

    fix #212 add licenses to cypress files

--- ui/cypress/e2e/main-tests-spec.cy.ts
+++ ui/cypress/e2e/main-tests-spec.cy.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 describe('Main Tests', () => {
     it('Visits the initial project page', () => {
         cy.visit('/')
@@ -9,7 +25,7 @@ describe('Main Tests', () => {
             cy.visit('/')
             cy.get('mat-row:nth-child(8) .link').first().click()
             cy.get('sg-overview-control button').last().click()
-            cy.wait(1000);
+            cy.wait(1000)
             cy.get('ngx-graph .node-group:nth-child(3)').click()
             cy.get('.spline-card-header__title').should('contain.text', 'my-other-job-output')
             cy.get('spline-data-widget .spline-data-record__value a').last().click()
